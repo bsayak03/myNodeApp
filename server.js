28ed2dd647c1cc3b5af90a817270b228d3045ef8
Sayak Bhattacharya <sayakofficial21@gmail.com>
2024-10-20 23:41:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello Folks, Its Sayak Bhattacharya this side! Building... ");
+    res.send("Hello Folks, Its Sayak Bhattacharya this side! Building... 123");
 })
 
 app.listen(8000, ()=>{
